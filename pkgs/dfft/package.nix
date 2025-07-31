19874f48ace7247d362615c99a06b2b6e95f14a9
Braian A. Diez <bdiez19@gmail.com>
2025-07-30 21:21:04 -0300

    feat(pkgs): added dfft, an amazing pkg

---
+++ pkgs/dfft/package.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+}:
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "dfft";
+  version = "0.1.1";
+
+  src = fetchFromGitHub {
+    owner = "dhth";
+    repo = "dfft";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-5JCGTrleCeJQ+pLiMyncLjm+m++lLCRxTmPRbAhgSOs=";
+  };
+
+  cargoHash = "sha256-mBvUzqXrpQyeZcIKhmA6v5rIoXIHTSG0sqisZwRTniw=";
+
+  nativeBuildInputs = [pkg-config];
+
+  meta = {
+    description = "Monitor changes as AI agents modify your codebase.";
+    homepage = "https://github.com/dhth/dfft";
+    changelog = "https://github.com/dhth/dfft/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.mit;
+    mainProgram = "dfft";
+  };
+})
