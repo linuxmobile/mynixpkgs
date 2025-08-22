3267b135903156f4825676b193f3c88133cc5172
Braian A. Diez <bdiez19@gmail.com>
2025-08-22 12:56:46 -0300

    fix(pkgs): crush

--- pkgs/crush/package.nix
+++ pkgs/crush/package.nix
@@ -1,28 +1,60 @@
 {
   lib,
-  buildGoModule,
+  buildGo125Module,
   fetchFromGitHub,
+  nix-update-script,
+  writableTmpDirAsHomeHook,
+  versionCheckHook,
 }:
-buildGoModule rec {
+buildGo125Module (finalAttrs: {
   pname = "crush";
-  version = "0.6.3";
+  version = "0.7.1";
 
   src = fetchFromGitHub {
     owner = "charmbracelet";
     repo = "crush";
-    tag = "v${version}";
-    hash = "sha256-lMp/qWSV+2+S8uH3TDPnldaBD5KhUdwyhvtn5j9TO1g=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-3wBGN3Uit1Xw8ZQ4WyR4PhA66Cfhl6YljMEbpaaJP60=";
   };
 
-  vendorHash = "sha256-vdzAVVGr7uTW/A/I8TcYW189E3960SCIqatu7Kb60hg=";
+  vendorHash = "sha256-qUcgVu6+cSFYDCsIB1pB5Vy3adWua2Rs8P9SNXJEjcA=";
 
-  doCheck = false;
+  # rename TestMain to prevent it from running, as it panics in the sandbox.
+  postPatch = ''
+    substituteInPlace internal/llm/provider/openai_test.go \
+      --replace-fail \
+        "func TestMain" \
+        "func DisabledTestMain"
+  '';
+
+  ldflags = [
+    "-s"
+    "-X=github.com/charmbracelet/crush/internal/version.Version=${finalAttrs.version}"
+  ];
+
+  checkFlags = let
+    # these tests fail in the sandbox
+    skippedTests = [
+      "TestOpenAIClientStreamChoices"
+      "TestGrepWithIgnoreFiles"
+      "TestSearchImplementations"
+    ];
+  in ["-skip=^${builtins.concatStringsSep "$|^" skippedTests}$"];
+
+  __darwinAllowLocalNetworking = true;
+
+  nativeCheckInputs = [writableTmpDirAsHomeHook];
+
+  nativeInstallCheckInputs = [versionCheckHook];
+  versionCheckProgramArg = "--version";
+  doInstallCheck = true;
+
+  updateScript = nix-update-script {};
 
   meta = {
-    description = "The AI coding agent for your favourite terminal.";
+    description = "Glamourous AI coding agent for your favourite terminal";
     homepage = "https://github.com/charmbracelet/crush";
-    changelog = "https://github.com/charmbracelet/crush/releases/tag/v${version}";
-    license = lib.licenses.mit;
+    changelog = "https://github.com/charmbracelet/crush/releases/tag/v${finalAttrs.version}";
     mainProgram = "crush";
   };
-}
+})
