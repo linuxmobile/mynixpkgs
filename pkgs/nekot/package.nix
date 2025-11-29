6ecba015b4ecef02abab2c365af9d74a95b9b641
Braian A. Diez <bdiez19@gmail.com>
2025-11-29 14:57:06 -0300

    chore(pkgs): update nekot

--- pkgs/nekot/package.nix
+++ pkgs/nekot/package.nix
@@ -3,15 +3,15 @@
   buildGoModule,
   fetchFromGitHub,
 }:
-buildGoModule rec {
+buildGoModule (finalAttrs: {
   pname = "nekot";
-  version = "0.6.0-unstable-2025-11-21";
+  version = "0.7.9";
 
   src = fetchFromGitHub {
     owner = "BalanceBalls";
     repo = "nekot";
-    rev = "bcf01952f79aa502c0b26351a97a10b874286b84";
-    hash = "sha256-Q9hIbcEJWmRLYi+1rt+8CldXnnG0tae9FQAOUl4++FY=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-bWosTrFmGyZbDR7l3Wp3ba3kK4qd9zpfiB46LiuV9mQ=";
   };
 
   vendorHash = "sha256-ey+lWmMoK8JS9PoOJWuVX6R0OqWrL4DU1Vihr6JXbOM=";
@@ -19,7 +19,7 @@ buildGoModule rec {
   ldflags = [
     "-s"
     "-w"
-    "-X=main.Version=${version}"
+    "-X=main.Version=${finalAttrs.version}"
   ];
 
   doCheck = false;
@@ -27,8 +27,8 @@ buildGoModule rec {
   meta = {
     description = "Powerful and intuitive terminal utility for interacting with both local and cloud LLMs";
     homepage = "https://github.com/BalanceBalls/nekot";
-    changelog = "https://github.com/BalanceBalls/nekot/releases/tag/v${version}";
+    changelog = "https://github.com/BalanceBalls/nekot/releases/tag/v${finalAttrs.version}";
     license = lib.licenses.mit;
     mainProgram = "nekot";
   };
-}
+})
