0b4b509e038744879ab48f235b23d498b280261d
Braian A. Diez <bdiez19@gmail.com>
2026-02-15 10:51:05 -0300

    feat(pkgs): helium update! vertical tabs exist

--- pkgs/helium/package.nix
+++ pkgs/helium/package.nix
@@ -36,11 +36,11 @@
 in
   stdenv.mkDerivation rec {
     pname = "helium";
-    version = "0.8.4.1";
+    version = "0.9.1.1";
 
     src = fetchurl {
       url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64_linux.tar.xz";
-      hash = "sha256-M/1wGewl500vJsoYfhbgXHQ4vlI6d0PRGGGGsRol6sc=";
+      hash = "sha256-B+2QNL32HWKPnc2sXiq8nz0Tz2TYZaBgGjDFSqu3nDo=";
     };
 
     sourceRoot = ".";
