13270b1428ed2977f28ab424a74ddaab02cfc7ca
Braian A. Diez <bdiez19@gmail.com>
2026-02-25 21:56:22 -0300

    feat(pkgs): add quickshell from noctalia
    Signed-off-by: Braian A. Diez <bdiez19@gmail.com>

---
+++ pkgs/quickshell/package.nix
@@ -0,0 +1,78 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  pkg-config,
+  cmake,
+  ninja,
+  spirv-tools,
+  qt6,
+  breakpad,
+  jemalloc,
+  cli11,
+  wayland,
+  wayland-protocols,
+  wayland-scanner,
+  libxcb,
+  libdrm,
+  libgbm,
+  pipewire,
+  pam,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "noctalia-qs";
+  version = "0.0.2";
+
+  src = fetchFromGitHub {
+    owner = "noctalia-dev";
+    repo = "noctalia-qs";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-rEuOibqZMx7+NvV2OVIdQQCDV9HT2lMsb9vHbA+d5FU=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    ninja
+    qt6.qtshadertools
+    spirv-tools
+    wayland-scanner
+    qt6.wrapQtAppsHook
+    pkg-config
+  ];
+
+  buildInputs = [
+    qt6.qtbase
+    qt6.qtdeclarative
+    qt6.qtwayland
+    qt6.qtsvg
+    cli11
+    wayland
+    wayland-protocols
+    libdrm
+    libgbm
+    breakpad
+    jemalloc
+    libxcb
+    pam
+    pipewire
+  ];
+
+  cmakeFlags = [
+    (lib.cmakeFeature "DISTRIBUTOR" "Nixpkgs")
+    (lib.cmakeBool "DISTRIBUTOR_DEBUGINFO_AVAILABLE" true)
+    (lib.cmakeFeature "INSTALL_QML_PREFIX" qt6.qtbase.qtQmlPrefix)
+    (lib.cmakeFeature "GIT_REVISION" "tag-v${finalAttrs.version}")
+  ];
+
+  cmakeBuildType = "RelWithDebInfo";
+  separateDebugInfo = true;
+  dontStrip = false;
+
+  meta = {
+    homepage = "https://quickshell.org";
+    description = "Flexbile QtQuick based desktop shell toolkit";
+    license = lib.licenses.lgpl3Only;
+    platforms = lib.platforms.linux;
+    mainProgram = "quickshell";
+  };
+})
