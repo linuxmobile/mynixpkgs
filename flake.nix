9b6e3a5811fccf1f47551c9398bf15c3cdbfbe28
Braian A. Diez <bdiez19@gmail.com>
2026-03-11 14:32:06 -0300

    refactor(flake): replace nixpkgs-unstable to nixos-unstable
    Signed-off-by: Braian A. Diez <bdiez19@gmail.com>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A series of nix packages";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
   };
 
