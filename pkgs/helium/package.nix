218a1d5c0171f1d61a2de47a649ebaa0c0e88a24
Braian A. Diez <bdiez19@gmail.com>
2026-04-28 18:37:29 -0300

    chore(pkgs): update helium
    Signed-off-by: Braian A. Diez <bdiez19@gmail.com>

--- pkgs/helium/package.nix
+++ pkgs/helium/package.nix
@@ -36,11 +36,11 @@
 in
   stdenv.mkDerivation rec {
     pname = "helium";
-    version = "0.11.5.1";
+    version = "0.11.6.1";
 
     src = fetchurl {
       url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64_linux.tar.xz";
-      hash = "sha256-wz9nqa0oU+M0Y0z8kWMCV7JCXhT4fHxhgCZB5yl78no=";
+      hash = "sha256-rJ9PIZTHU7wzndAqddknH1Y6U1j5JLOtE1NtaqSrx7w=";
     };
 
     sourceRoot = ".";
